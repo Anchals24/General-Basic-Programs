d51e3a6f086ad1f415a5bff7e91a196b3848bed6
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-16 11:07:28 +0530

    Two Dimensional Array

---
+++ #2_Dimensional_Array.py
@@ -0,0 +1,24 @@
+#2_Dimensional_Array
+#How to access the elements of a 2D Matrix?
+
+M = [[1,2] , [2,3,4] , [2,3,2]]
+for m in M:
+    for c in m:
+        print(c, end= " ")
+    print()
+
+#How to take a 2D Array as an input?
+
+#row 14 means n = number of rows.
+
+n = int(input()) 
+ans = []  
+for i in range(n):
+    rows = input()
+    rows = rows.split()
+    r = []
+    for x in rows:
+        r.append(int(x))
+    ans.append(r)
+print(ans)
+
