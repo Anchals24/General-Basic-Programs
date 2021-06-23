ecea678f194a831d31f1ad47b999dc6ebf177a2b
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-23 14:39:56 +0530

    Modules in Python.

---
+++ Modules in Python.py
@@ -0,0 +1,11 @@
+import moduleabc
+
+moduleabc.display()
+moduleabc.trial2()
+ans = moduleabc.myadd(2,7)
+print(ans)
+
+
+
+# A python module is a file containing definitions and statements
+# Conventional modules are named using lowercase letters.Once created we can then use these by simply using import in python main file and then simply calling the function.
